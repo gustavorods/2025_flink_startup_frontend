e5396a9cf587bf19f665b5fe426636243a037cae
gustavorods <gustavo.rlsilva07@gmail.com>
2025-04-30 16:59:55 -0300

    fix: merge conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,17 @@
 import './App.css';
-<<<<<<< HEAD
 import { FileButton, FirstLink, FirstButton } from './components'
 import {BrowserRouter} from 'react-router-dom'
 import { Router } from './Router'
 
-=======
 import { FileButton, FirstLink, NavbarSemLogin } from './components'
->>>>>>> ea9f3222b4815f689805f66293fdc5cb4d67d908
-
 const App = () => {
  
   return (
     <>
-<<<<<<< HEAD
       <BrowserRouter>
-        <Router/>
+        <Router />
       </BrowserRouter>
-=======
-    <NavbarSemLogin />
->>>>>>> ea9f3222b4815f689805f66293fdc5cb4d67d908
+      <NavbarSemLogin />
     </>
   )
 }
