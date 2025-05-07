89017b84db4a7f3b9a3bab679ce4b0c584a4fb91
gustavorods <gustavo.rlsilva07@gmail.com>
2025-05-07 15:25:58 -0300

    fix: sportbutton

--- src/components/SportButton/SportButton.jsx
+++ src/components/SportButton/SportButton.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import { FirstButton} from '../../components'
 import styles from './SportButton.module.css'
 
-const SportButton = ({ label, selected, onClick }) => {
+const SportButton = ({ label, selected, onClick, className='' }) => {
   return (
     <FirstButton
       texto={label}
       cor={selected ? '#388E3C' : '#fff'}
-      tamanho="1rem"
+      tamanho="1.2rem"
       onClick={onClick}
-      className={`${styles.buttonBase} ${selected ? styles.selected : styles.unselected}`}
+      className={`${styles.buttonBase} ${selected ? styles.selected : styles.unselected} ${className}`}
     />
   )
 }
